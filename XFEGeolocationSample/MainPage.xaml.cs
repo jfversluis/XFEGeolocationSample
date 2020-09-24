5da9bc428b175d345b5593fb8e66cb8862db1435
Gerald Versluis <gerald@verslu.is>
2020-09-24 09:32:57 +0200

    Implement Geolocation

--- XFEGeolocationSample/MainPage.xaml.cs
+++ XFEGeolocationSample/MainPage.xaml.cs
@@ -4,15 +4,37 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace XFEGeolocationSample
 {
     public partial class MainPage : ContentPage
     {
+        bool isGettingLocation;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            isGettingLocation = true;
+
+            while (isGettingLocation)
+            {
+                var result = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Default, TimeSpan.FromMinutes(1)));
+
+                resultLocation.Text += $"lat: {result.Latitude}, lng: {result.Longitude}{Environment.NewLine}";
+
+                await Task.Delay(1000);
+            }
+        }
+
+        void Button1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            isGettingLocation = false;
         }
     }
 }
